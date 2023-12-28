c5cc7cc0f8d0120348097d6d69f7d9552ecedeb9
bathan1 <side_nathan@hotmail.com>
2023-12-28 02:33:07 -0800

    Refactor functions

--- main.js
+++ main.js
@@ -1,43 +1,44 @@
 const track = document.getElementById("image-track");
 
 function setNextPercentage(delta) {
-
-};
-
-const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
-
-const handleOnUp = () => {
-  track.dataset.mouseDownAt = "0";  
-  track.dataset.prevPercentage = track.dataset.percentage;
-}
-
-const handleOnMove = e => {
-  if(track.dataset.mouseDownAt === "0") return;
-  
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
   const maxDelta = window.innerWidth / 2;
-  
-  const percentage = (mouseDelta / maxDelta) * -100;
-  const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
-  const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  const percentage = (delta / maxDelta) * -100;
+  const nextPercentageNotBound = parseFloat(track.dataset.prevPercentage) + percentage;
+  const nextPercentage = Math.max(Math.min(nextPercentageNotBound, 0), -100);
   
   track.dataset.percentage = nextPercentage;
-  
   track.animate({
     transform: `translate(${nextPercentage}%, -50%)`
   }, { duration: 1200, fill: "forwards" });
   
-  for(const image of track.getElementsByClassName("image")) {
+  for (const image of track.getElementsByClassName("image")) {
     image.animate({
       objectPosition: `${100 + nextPercentage}% center`
     }, { duration: 1200, fill: "forwards" });
   }
+};
+
+function handleMouseDown(e) {
+  track.dataset.mouseDownAt = e.clientX;
+}
+
+function handleMouseUp() {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+function handleMouseMove(e) {
+  if (track.dataset.mouseDownAt === "0") {
+    return;
+  }
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+  setNextPercentage(mouseDelta);
 }
 
 let cumulativeScrollDelta = 0; // This will accumulate the scroll delta
 let scrollTimeout;
 
-const handleScroll = e => {
+function handleScroll(e) {
   const scrollSensitivity = 0.33;
   
   // Accumulate the scroll delta
@@ -56,42 +57,22 @@ const handleScroll = e => {
   const maxDelta = window.innerWidth / 2;
   cumulativeScrollDelta = Math.min(cumulativeScrollDelta, maxDelta);
 
-  const percentage = (cumulativeScrollDelta / maxDelta) * -100;
-  const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
-  const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-  
-  track.dataset.percentage = nextPercentage;
-  
-  track.animate({
-    transform: `translate(${nextPercentage}%, -50%)`
-  }, { duration: 1200, fill: "forwards" });
-  
-  for (const image of track.getElementsByClassName("image")) {
-    image.animate({
-      objectPosition: `${100 + nextPercentage}% center`
-    }, { duration: 1200, fill: "forwards" });
-  }
+  setNextPercentage(cumulativeScrollDelta);
 
   clearTimeout(scrollTimeout);
   scrollTimeout = setTimeout(() => {
     cumulativeScrollDelta = 0;
-    track.dataset.prevPercentage = nextPercentage;
+    track.dataset.prevPercentage = track.dataset.percentage;
   }, 300);
 };
 
+// Bind event listeners to window
 window.addEventListener("wheel", handleScroll);
+window.addEventListener("mousedown", handleMouseDown);
+window.addEventListener("mouseup", handleMouseUp);
+window.addEventListener("mousemove", handleMouseMove);
 
-/* -- Had to add extra lines for touch events -- */
-
-window.onmousedown = e => handleOnDown(e);
-
-window.ontouchstart = e => handleOnDown(e.touches[0]);
-
-window.onmouseup = e => handleOnUp(e);
-
-window.ontouchend = e => handleOnUp(e.touches[0]);
-
-window.onmousemove = e => handleOnMove(e);
-
-window.ontouchmove = e => handleOnMove(e.touches[0]);
-
+// Add touch capabilities
+window.ontouchstart = e => handleMouseDown(e.touches[0]);
+window.ontouchend = e => handleMouseUp(e.touches[0]);
+window.ontouchmove = e => handleMouseMove(e.touches[0]);
