77937ee4840a62b60b933532a290a289681e576c
bathan1 <side_nathan@hotmail.com>
2023-12-28 02:09:45 -0800

    Initial commit - got scroll and mouse drag synchronized

---
+++ main.js
@@ -0,0 +1,91 @@
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+  const maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100;
+  const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
+  const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+let cumulativeScrollDelta = 0; // This will accumulate the scroll delta
+let scrollTimeout;
+
+const handleScroll = e => {
+  const scrollSensitivity = 0.33;
+  
+  // Accumulate the scroll delta
+  if (e.deltaX) {
+    cumulativeScrollDelta += e.deltaX * scrollSensitivity;
+  } else {
+    cumulativeScrollDelta += e.deltaY * scrollSensitivity;
+  }
+
+  // Get the 
+  const inverse = (track.dataset.prevPercentage / 100) * (window.innerWidth / 2);
+  cumulativeScrollDelta = Math.max(cumulativeScrollDelta, inverse);
+  const maxDelta = window.innerWidth / 2;
+  if (cumulativeScrollDelta > maxDelta) {
+    cumulativeScrollDelta = maxDelta;
+  }
+  const percentage = (cumulativeScrollDelta / maxDelta) * -100;
+  const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
+  const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for (const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+
+  clearTimeout(scrollTimeout);
+  scrollTimeout = setTimeout(() => {
+    cumulativeScrollDelta = 0;
+    track.dataset.prevPercentage = nextPercentage;
+  }, 1000);
+};
+
+window.addEventListener("wheel", handleScroll);
+
+/* -- Had to add extra lines for touch events -- */
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
