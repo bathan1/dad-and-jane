12d56783514a83bdeb5b0f8071cc991cfde2577c
bathan1 <side_nathan@hotmail.com>
2024-01-03 00:46:39 -0800

    Config for final push hopefully setup

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        korea: resolve(__dirname, 'src/korea.html'),
+        food: resolve(__dirname, 'src/food.html'),
+        wedding: resolve(__dirname, 'src/wedding.html'),
+        misc: resolve(__dirname, 'src/misc.html'),
+      },
+    },
+  },
+})
